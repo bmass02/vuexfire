fae15e6acc453b625dc9609170e3c5ceeccd0c1c
Bryan Massoth <btmassoth@gmail.com>
2017-11-07 18:23:51 -0500

    Added tests for Firestore.

---
+++ test/bind-doc.spec.js
@@ -0,0 +1,70 @@
+import test from 'ava'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {
+  createCollectionRef,
+  createFirebaseApp,
+} from './helpers/firebase'
+
+import {
+  firebaseMutations,
+  firebaseAction,
+} from '../src'
+
+const firebaseApp = createFirebaseApp()
+
+test.before(t => {
+  Vue.use(Vuex)
+})
+
+test.beforeEach(async (t) => {
+  t.context.store = new Vuex.Store({
+    state: {
+      options: null,
+      primitive: null,
+    },
+    actions: {
+      setPrimitiveRef: firebaseAction(({ bindFirestoreRef }, ref) => {
+        bindFirestoreRef('primitive', ref)
+      }),
+      setOptionsRef: firebaseAction(({ bindFirestoreRef }, ref) => {
+        bindFirestoreRef('options', ref)
+      }),
+      unbindOptionsRef: firebaseAction(({ unbindFirebaseRef }) => {
+        unbindFirebaseRef('options')
+      }),
+    },
+    mutations: firebaseMutations,
+  })
+
+  // Create a fresh ref for the test
+  const collectionRef = await createCollectionRef(firebaseApp)
+  t.context.ref = collectionRef.doc('vuexfire')
+})
+
+test('binds doc to state', async (t) => {
+  t.context.store.dispatch('setOptionsRef', t.context.ref)
+  await t.context.ref.set({
+    key: 'key1',
+    array: [
+      '1',
+      '2',
+    ],
+    object: {
+      nestedKey: 'key2',
+    },
+  })
+  await t.context.ref.get()
+
+  t.deepEqual(t.context.store.state.options, {
+    '.id': t.context.ref.id,
+    key: 'key1',
+    array: [
+      '1',
+      '2',
+    ],
+    object: {
+      nestedKey: 'key2',
+    },
+  })
+})
